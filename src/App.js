0eb5a9650e95facb0a1331d2fb942fcedf1b6e60
Igor <ibokalo96@gmail.com>
2021-01-25 14:31:51 +0200

    keys,
    Робота зі списком

--- src/App.js
+++ src/App.js
@@ -31,12 +31,10 @@ class App extends Component {
       textAlign:'center'
     }
 
-    const cars = this.state.cars
 
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1> 
-        {/* <h1>TESTING</h1> */}
 
         <input type="text" onChange={this.handleInput} />
 
@@ -44,22 +42,17 @@ class App extends Component {
          onClick={this.changeTitleHandler.bind(this, 'Changed!')}
         >Change title</button>
 
-      <Car
-       name={cars[0].name} 
-       year={cars[0].year}
-       onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
-      />
-      <Car
-       name={cars[1].name} 
-       year={cars[1].year}
-       onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
-      />
-      <Car
-       name={cars[2].name}
-       year={cars[2].year}
-       onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
+        { this.state.cars.map((car, index) => {
+          return (
+            <Car 
+              key={index}
+              name={car.name}
+              year={car.year}
+              onChangeTitle={() => this.changeTitleHandler(car.name)}
+            />
+          )
+        }) }
 
-      />
     </div>
       
     );
