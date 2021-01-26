5dba87a1fae0f06eee0e06cb932fd619f56e07dd
Igor <ibokalo96@gmail.com>
2021-01-26 13:03:12 +0200

    btn.delete
    delete dynamic

--- src/App.js
+++ src/App.js
@@ -19,10 +19,21 @@ class App extends Component {
       showCars: !this.state.showCars
     })
   }
+  onChangeName (name, index) {
+    
+    const car = this.state.cars[index]
+    car.name = name
+    const cars = [...this.state.cars]
+    cars[index] = car
+    this.setState({cars})
+    }
 
-  changeTitleHandler = pageTitle => {
-    this.setState({pageTitle})
-  }
+    deleteHandler(index) {
+      const cars = this.state.cars.concat()
+      cars.splice(index, 1)
+
+      this.setState({cars})
+    }
 
   render() {
     const divStyle = {
@@ -38,7 +49,8 @@ class App extends Component {
             key={index}
             name={car.name}
             year={car.year}
-            onChangeTitle={() => this.changeTitleHandler(car.name)}
+            onDelete={this.deleteHandler.bind(this, index)}
+            onChangeName={event => this.onChangeName(event.target.value, index)}
           />
         )
       })
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -9,10 +9,16 @@ import React from 'react';
 // const Car = () => <div>This is car component</div>
 
 export default props => (
-    <div>
+    <div style = {{
+      border: '1px solid #ccc',
+      marginBottom: '10px',
+      display: 'block',
+      padding: '10px'
+    }}>
       <h3>Car name: {props.name}</h3>  
       <p>Year: <strong>{props.year}</strong></p> 
-      <button onClick={props.onChangeTitle}>Click</button>
+      <input type= "text" onChange={props.onChangeName} value={props.name}/>
+      <button onClick={props.onDelete}>Delete</button>
     </div>
 )
 
